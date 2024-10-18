e269deb3b5220e01ce2c0c452b4606dd96d53fa4
smabari <mdazizulbarishafin@gmail.com>
2024-10-18 06:07:51 +0600

    tried to make the nav

--- src/Components/partials/Nav.jsx
+++ src/Components/partials/Nav.jsx
@@ -1,6 +1,8 @@
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className="px-6 py-4 flex bg-sky-400">
+      <p className="text-yellow-300 text-xl font-semibold">TONYS.CARPET</p>
+    </div>
   )
 }
 export default Nav
